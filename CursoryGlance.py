eb8804a01607999aba9bb906d43d41dbc4c9347b
LinCocoon <xuenibian@gmail.com>
2021-07-20 13:35:34 +0100

    Alignment Surveying

--- CursoryGlance.py
+++ CursoryGlance.py
@@ -1,4 +1,7 @@
 # First look at the whole dataset
+
+def LinkaGlance(df):
+    
     AllColumns = df.columns.values.tolist()
     for Col in AllColumns:
         if len(df[Col].dropna().unique()) <= 1:
@@ -7,26 +10,27 @@
             # or all NaNs + one other unique value
             # e.g. print(df['class'].dropna().unique())
             # returns 'Insecta'
+            print(f"Dropped column: {col}")
         else:
             NaNCount = (df[Col].isna()).sum()
             DataCount = len(df) - NaNCount
             
-        print (f"{Col}: {DataCount}/{len(df)} not NaNs")
+        print(f"{Col}: {DataCount}/{len(df)} not NaNs")
         print(f"{len(df[Col].dropna().unique())} unique not NaN entries.")
         print("\n")
 
-    df = df.set_index(['family']).sort_index()
-    # 3015/3179 not NaNs with 127 unique not NaN entries.
-    print(df.index.value_counts()[0:10])
-    # Top ten families
+# df = df.set_index(['family']).sort_index()
+# 3015/3179 not NaNs with 127 unique not NaN entries.
+# print(df.index.value_counts()[0:10])
+# Top ten families
 
-    CurculionidaeDF = df[df.index == 'Curculionidae'] # 511 entries
-    StaphylinidaeDF = df[df.index == 'Staphylinidae'] # 385 entries
-    CerambycidaeDF = df[df.index == 'Cerambycidae']   # 164 entries
-    ScarabaeidaeDF = df[df.index == 'Scarabaeidae']   # 127 entries
-    CarabidaeDF = df[df.index == 'Carabidae']         # 115 entries
-    TenebrionidaeDF = df[df.index == 'Tenebrionidae'] # 81 entries
-    MordellidaeDF = df[df.index == 'Mordellidae']     # 76 entries
-    CoccinellidaeDF = df[df.index == 'Coccinellidae'] # 71 entries
-    NitidulidaeDF = df[df.index == 'Nitidulidae']     # 70 entries
-    ElateridaeDF = df[df.index == 'Elateridae']       # 50 entries
+    # CurculionidaeDF = df[df.index == 'Curculionidae'] # 511 entries
+    # StaphylinidaeDF = df[df.index == 'Staphylinidae'] # 385 entries
+    # CerambycidaeDF = df[df.index == 'Cerambycidae']   # 164 entries
+    # ScarabaeidaeDF = df[df.index == 'Scarabaeidae']   # 127 entries
+    # CarabidaeDF = df[df.index == 'Carabidae']         # 115 entries
+    # TenebrionidaeDF = df[df.index == 'Tenebrionidae'] # 81 entries
+    # MordellidaeDF = df[df.index == 'Mordellidae']     # 76 entries
+    # CoccinellidaeDF = df[df.index == 'Coccinellidae'] # 71 entries
+    # NitidulidaeDF = df[df.index == 'Nitidulidae']     # 70 entries
+    # ElateridaeDF = df[df.index == 'Elateridae']       # 50 entries
--- SequencePrepper.py
+++ SequencePrepper.py
@@ -3,7 +3,7 @@
 # Description: Linka's sequence prepper
 
 
-# I want this tool to:
+# I want this to:
 # Look here in this database -Database
 # Look here in this fasta for the sequences -FastaIn
 # but only let in almost complete sequences -MinCompletion
@@ -30,17 +30,27 @@ def run(args):
     CompCutoff = args.MinCompletion
     TargetFamilies = args.Families.split(' ')
     FastaOut = args.FastaOut
-    
+
+    print("\n")
     DF = AppendSeqCompletion(PathToDB, PathToFastaIn)
-    print(len(DF))
+    print(f"Read in full database with {len(DF)} entries.")
+
+    print("\n")
     DFRemoveNaNs = CleanData(DF, CompCutoff)
-    print(len(DFRemoveNaNs))
+    print("Dropped entries without sequences")
+    print("Dropped sequences that are less than {CompCutoff*100}% complete.")
+    print("Dropped sequences with no taxonomic membership whatsoever.")
+    print(f"Now working on cleaned database with {len(DFRemoveNaNs)} entries.")
+
+    print("\n")
     DFCherryPicked = DFRemoveNaNs.loc[
         DFRemoveNaNs['family'].isin(TargetFamilies)
     ]
-    print(len(DFCherryPicked))
+    print(f"Filtered for families: {TargetFamilies}.")
+    print(f"Now working on cherrypicked database with {len(DFCherryPicked)} entries.")
     
     WriteRenamedFasta(DFCherryPicked, PathToFastaIn, FastaOut)
+
 
 #========================================================================
 
@@ -49,7 +59,7 @@ def main():
     parser = argparse.ArgumentParser(description ="python SequencePrepper.py -Database *.csv -FastaIn Supermatrix5079Seq.fa -Families 'Staphylinidae Elateridae' -FastaOut Test.fa")
     parser.add_argument("-Database", help="Relative path of database containing renaming references.", required=True, dest="Database", type=str)
     parser.add_argument("-FastaIn", help="Relative path of fasta containing the actual sequences.", required=True, dest="FastaIn", type=str)
-    parser.add_argument("-MinCompletion", help="Required percentage completion of sequences as float e.g. 0.50.", default=0.50, dest="MinCompletion", type=float)
+    parser.add_argument("-MinCompletion", help="Required percentage completion of sequences as float. Default 0.50.", default=0.50, dest="MinCompletion", type=float)
     parser.add_argument("-Families", help="The list of families wanted in the outfile. List must be quoted and separated by a single space, like 'Staphylinidae Elateridae Nitidulidae'.", required=True, dest="Families", type=str)
     parser.add_argument("-FastaOut", help="Name of output fasta to be written at the end.", required=True, dest="FastaOut", type=str)
     parser.set_defaults(func=run)
